f3197c0ab81e07ae0c9243cf98ff1c5209a5fd5f
manasi2810 <manasinikam102*gail.com>
2026-09-10 11:16:37 +0530

    Fix React Router base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -18,8 +18,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <BrowserRouter basename="/recipe-app">
+  <App />
+</BrowserRouter>
   </StrictMode>,
 )
